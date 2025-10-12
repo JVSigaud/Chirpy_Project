949255f5d259be44fb29bf89299d61180ab04378
JVSigaud <sigaud@poli.ufrj.br>
2025-10-12 16:58:24 -0300

    A: server init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module Chirpy_Project
+
+go 1.25.2
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	serveMux := http.NewServeMux()
+	var server = &http.Server{
+		Addr:    ":8080",
+		Handler: serveMux,
+	}
+	err := server.ListenAndServe()
+
+	if err != nil {
+		fmt.Sprintf("Erro: %v", err)
+	}
+
+}
