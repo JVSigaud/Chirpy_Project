015177aec0cf73336f3a4f8e32a34ec68aa7074c
JVSigaud <sigaud@poli.ufrj.br>
2025-10-12 18:12:37 -0300

    B: add FileServer

--- main.go
+++ main.go
@@ -7,14 +7,14 @@ import (
 
 func main() {
 	serveMux := http.NewServeMux()
+	serveMux.Handle("/", http.FileServer(http.Dir(".")))
 	var server = &http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
-	err := server.ListenAndServe()
-
-	if err != nil {
-		fmt.Sprintf("Erro: %v", err)
+	fmt.Println("Server running at http://localhost:8080")
+	if err := server.ListenAndServe(); err != nil {
+		// ListenAndServe always returns a non-nil error
+		fmt.Println("Server error:", err)
 	}
-
 }
