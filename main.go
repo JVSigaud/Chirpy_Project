7706901bee0425ed832867d9bf910def0ef5f500
JVSigaud <sigaud@poli.ufrj.br>
2025-10-13 18:55:01 -0300

    D: add counter

--- main.go
+++ main.go
@@ -3,16 +3,49 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	// ...
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("Hits: " + fmt.Sprintf("%d", cfg.fileserverHits.Load())))
+}
+func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+
+}
+
 func main() {
+	apicfg := &apiConfig{}
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	serveMux.Handle("/app/", apicfg.middlewareMetricsInc(
+		http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+
 	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
 	})
+
+	serveMux.HandleFunc("/metrics", apicfg.handleMetrics)
+	serveMux.HandleFunc("/reset", apicfg.resetMetrics)
+
 	var server = &http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
