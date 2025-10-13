71dbd375f79f1d1b253d62401c7aa49f3b7f0cf2
JVSigaud <sigaud@poli.ufrj.br>
2025-10-13 16:47:51 -0300

    C: add endpoint

--- main.go
+++ main.go
@@ -7,16 +7,20 @@ import (
 
 func main() {
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
-	serveMux.Handle("/logo.png", http.FileServer(http.Dir("/assets/")))
-
+	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
 	var server = &http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
 	fmt.Println("Server running at http://localhost:8080")
 	if err := server.ListenAndServe(); err != nil {
-		// ListenAndServe always returns a non-nil error
+		// ListenAndServe always returns a non-nil erro
+		// r
 		fmt.Println("Server error:", err)
 	}
 }
